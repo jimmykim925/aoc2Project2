c3c5236d52ea5cac9eed54eaecebf49cf4943974
Jimmy Kim <jimmykim925@fullsail.edu>
2012-09-30 23:31:01 -0700

    added initial files, ui buttons, labels

---
+++ aoc2Week2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aoc2Week2
+//
+//  Created by Jimmy Kim on 9/30/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+-(IBAction)onClick:(id)sender;
+
+@end
---
+++ aoc2Week2/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  aoc2Week2
+//
+//  Created by Jimmy Kim on 9/30/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+-(IBAction)onClick:(id)sender
+{
+  NSLog(@"click");
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
