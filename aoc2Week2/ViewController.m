f1ddbd2f7e94ae4cbc069c12f0a0e970a4eeb253
Jimmy Kim <jimmykim925@fullsail.edu>
2012-10-01 00:52:13 -0700

    added UIButton, switch statement for buttons

--- aoc2Week2/ViewController.m
+++ aoc2Week2/ViewController.m
@@ -22,7 +22,55 @@
 
 -(IBAction)onClick:(id)sender
 {
-  NSLog(@"click");
+  UIButton *button = (UIButton *)sender;
+  if (button != nil){
+    switch ([button tag]) {
+      case 0:
+        NSLog(@"0");
+        break;
+      case 1:
+        NSLog(@"1");
+        break;
+      case 2:
+        NSLog(@"2");
+        break;
+      case 3:
+        NSLog(@"3");
+        break;
+      case 4:
+        NSLog(@"4");
+        break;
+      case 5:
+        NSLog(@"5");
+        break;
+      case 6:
+        NSLog(@"6");
+        break;
+      case 7:
+        NSLog(@"7");
+        break;
+      case 8:
+        NSLog(@"8");
+        break;
+      case 9:
+        NSLog(@"9");
+        break;
+      case 10:
+        NSLog(@"10");
+        break;
+      case 11:
+        NSLog(@"11");
+        break;
+      case 12:
+        NSLog(@"12");
+        break;
+      
+      default:
+        break;
+    }
+   
+  }
+  
 }
 
 - (void)didReceiveMemoryWarning
